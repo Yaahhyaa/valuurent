8c421c1cb089e30ac7fd14ab4579bc479b3cb84f
Yaahhyaa <yahya.shekh@student.htldornbirn.at>
2024-03-13 08:54:44 +0100

    commit

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,16 +2,20 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.mygdx.game.actors.Spieler;
+import com.mygdx.game.Character.Spieler;
 import com.mygdx.game.helper.imageHelper;
-
+import org.w3c.dom.Text;
 
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
+    Texture mapTexture;
+    Texture spielerTexture;
     Texture img;
     Music backgroundMusic; // Hintergrundmusik
 
@@ -19,41 +23,50 @@ public class MyGdxGame extends ApplicationAdapter {
 
     @Override
     public void create() {
-
         batch = new SpriteBatch();
-        img = new Texture("images/Map.png");
+        mapTexture = new Texture("images/Map.png");
         img = new Texture("images/jett.png");
 
-
         // Hintergrundmusik
-        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("Music/Valorant Main Menu .mp3"));
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("Music/Valorant_Main_Menu.mp3"));
         backgroundMusic.setLooping(true);
         backgroundMusic.setVolume(0.5f);
         backgroundMusic.play();
+
         imageHelper ih = new imageHelper();
 
-        spieler = new Spieler(0, 0, ih.changeImgSize(250, 150, "images/jett.png"));
+
     }
 
     @Override
     public void render() {
 
+        float delta = Gdx.graphics.getDeltaTime();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)&& spieler.getX()<750) spieler.move(1);
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)&& spieler.getX()>0) spieler.move(0);
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)&& spieler.getY()<580) spieler.move(2);
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)&& spieler.getY()>0) spieler.move(3);
+
+
         ScreenUtils.clear(1, 0, 0, 1);
         batch.begin();
 
         // Zuerst die Karte im Hintergrund zeichnen
-        batch.draw(img, 0, 0);
+        batch.draw(mapTexture, 0, 0);
 
         // Dann den Spieler (Jett) im Vordergrund zeichnen
-        batch.draw(spieler.getTexture(), spieler.getX(), spieler.getY());
 
         batch.end();
+
+
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
+        mapTexture.dispose();
+        spielerTexture.dispose();
         backgroundMusic.dispose();
     }
 }
