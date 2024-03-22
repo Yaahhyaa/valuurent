c2d65ed209ee66aa7396d18201a7aa39ed5f4a30
Abuudii <Abdalla.nada@student.htldornbirn.at>
2024-03-22 09:10:54 +0100

    kaesroöadf

--- core/src/com/mygdx/game/Character/Spieler.java
+++ core/src/com/mygdx/game/Character/Spieler.java
@@ -1,18 +1,33 @@
 package com.mygdx.game.Character;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+
 public class Spieler extends SpielObjekt{
     private Rectangle boundary;
     private float speed = 2f;
     private float acceleration = 0.5f;
     private int direction = 0;
+
+    private Animation<TextureRegion> animation;
+    private TextureRegion currentFrame;
+    private TextureAtlas atlas;
+    private boolean isAnimation = true;
+    private float stateTime =0;
     public Spieler(int x, int y, Texture image) {
         super(x, y, image);
         boundary = new Rectangle();
         this.setBoundary();
+        atlas = new TextureAtlas(Gdx.files.internal("animation/jet.atlas"));
+        Array<TextureAtlas.AtlasRegion> frames = atlas.findRegions("a");
+        animation = new Animation<>(0.1f, frames, Animation.PlayMode.LOOP);
 }
     public Rectangle getBoundary() {
         return boundary;
@@ -25,11 +40,16 @@ public class Spieler extends SpielObjekt{
     @Override
 
     public void draw(Batch b, float parentAlpha) {
-        this.getImage().draw(b);
+
+        //this.getImage().draw(b);
+        if (isAnimation) {
+            currentFrame = animation.getKeyFrame(stateTime, true);
+            b.draw(currentFrame, this.getX(), this.getY());
+        }
     }
 
     public void update(float delta){
-
+        stateTime += delta;
     }
 
 
--- core/src/com/mygdx/game/Screens/GameScreen2.java
+++ core/src/com/mygdx/game/Screens/GameScreen2.java
@@ -2,16 +2,18 @@ package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.Character.GameActor;
 import com.mygdx.game.Character.Spieler;
-import com.mygdx.game.helper.imageHelper;
-import org.w3c.dom.Text;
 
 public class GameScreen2 implements Screen {
     SpriteBatch batch;
@@ -25,12 +27,13 @@ public class GameScreen2 implements Screen {
     GameActor gameActor;
     GameActor background;
 
+
+
+
     public GameScreen2(Game aGame) {
         game = aGame;
         stage = new Stage(new ScreenViewport());
         initStage();
-
-
     }
 
     @Override
@@ -40,12 +43,15 @@ public class GameScreen2 implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) spieler.move(1);
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) spieler.move(0);
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) spieler.move(2);
         if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) spieler.move(3);
 
+        stage.act(delta);
         stage.draw();
 
     }
@@ -77,6 +83,7 @@ public class GameScreen2 implements Screen {
         spielerTexture.dispose();
         backgroundMusic.dispose();
     }
+
     private void initStage(){
         //mapTexture = new Texture("images/Map.png");
         //img = new Texture("images/jett.png");
@@ -87,16 +94,19 @@ public class GameScreen2 implements Screen {
         backgroundMusic.setVolume(0.5f);
         backgroundMusic.play();
 
-        spieler = new Spieler(0,0, new Texture("images/jett.png"));
+        batch = new SpriteBatch();
 
-        gameActor = new GameActor(0, 0, new Texture("images/jett.png")); // Hier den gameActor initialisieren
+
+        spieler = new Spieler(100, 100, new Texture("images/jett.png"));
+
+        //gameActor = new GameActor(0, 0, new Texture("images/jett.png")); // Hier den gameActor initialisieren
         background = new GameActor(0,0,new Texture("images/Map.png"));
         //feind = new Feind(500,300,"images/wi_rund_20.png",Gdx.graphics.getHeight());
 
         stage.addActor(background);
-        stage.addActor(gameActor);
+        //stage.addActor(gameActor);
         stage.addActor(spieler);
         //stage.addActor(feind);
-    }
 
+    }
 }
